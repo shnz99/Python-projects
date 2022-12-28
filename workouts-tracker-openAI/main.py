e715b90d11890c4ab5d97cc0ecc8c97b088f6083
damiangaw <d.gawin@pm.me>
2022-12-28 11:47:19 +0100

    update

--- workouts-tracker-openAI/main.py
+++ workouts-tracker-openAI/main.py
@@ -4,30 +4,38 @@ from datetime import datetime
 
 x_app_id = os.environ.get("X-APP-ID")
 x_app_key = os.environ.get("X-APP-KEY")
-headers = {"x-app-id": x_app_id, "x-app-key": x_app_key}
+nutri_headers = {"x-app-id": x_app_id, "x-app-key": x_app_key}
 nutri_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_enpoint = (
     "https://api.sheety.co/e0c39ab4947543a4ce2ba49a5de8751b/myWorkouts/workouts"
 )
-today = (datetime.now()).strftime("%d/%m/%Y")
+date = (datetime.now()).strftime("%d/%m/%Y")
+time = (datetime.now()).strftime("%H:%M:%S")
 
-sheety_config = {
-    "workout": {
-        "date": "Date",
-        "time": "Time",
-        "exercise": "exe",
-        "duration": "dur",
-        "calories": "cal",
-    }
+query = input("Tell me which exercises you did: ")
+nutri_config = {
+    "query": query,
 }
+nutri_request = requests.post(
+    url=nutri_endpoint,
+    json=nutri_config,
+    headers=nutri_headers,
+)
+nutri_request.raise_for_status()
+result = nutri_request.json()
+for exercise in result["exercises"]:
+    exercise = result["exercises"][exercise]["name"]
+    duration = result["exercises"][exercise]["duration_min"]
+    calories = result["exercises"][exercise]["nf_calories"]
+    # "name", "duration_min", "nf_calories"
 
-# query = input("Tell me which exercises you did: ")
-# nutri_config = {
-#     "query": query,
-# }
-# request = requests.post(
-#     url=nutri_endpoint,
-#     json=nutri_config,
-#     headers=headers,
-# )
-# print(request.text)
+    sheety_config = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise.title(),
+            "duration": duration,
+            "calories": calories,
+        }
+    }
+    sheety_post = requests.post(url=sheety_enpoint, json=sheety_config)
