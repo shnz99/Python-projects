d54511b5721a2ab0c1b77692338de5676514ec84
damiangaw <d.gawin@pm.me>
2022-12-28 11:22:32 +0100

    update config to google sheets

--- workouts-tracker-openAI/main.py
+++ workouts-tracker-openAI/main.py
@@ -1,22 +1,33 @@
-from email import header
 import os
-from traceback import print_tb
 import requests
+from datetime import datetime
 
 x_app_id = os.environ.get("X-APP-ID")
 x_app_key = os.environ.get("X-APP-KEY")
 headers = {"x-app-id": x_app_id, "x-app-key": x_app_key}
+nutri_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_enpoint = (
     "https://api.sheety.co/e0c39ab4947543a4ce2ba49a5de8751b/myWorkouts/workouts"
 )
+today = (datetime.now()).strftime("%d/%m/%Y")
 
-query = input("Tell me which exercises you did: ")
-config = {
-    "query": query,
+sheety_config = {
+    "workout": {
+        "date": "Date",
+        "time": "Time",
+        "exercise": "exe",
+        "duration": "dur",
+        "calories": "cal",
+    }
 }
-request = requests.post(
-    url="https://trackapi.nutritionix.com/v2/natural/exercise",
-    json=config,
-    headers=headers,
-)
-print(request.text)
+
+# query = input("Tell me which exercises you did: ")
+# nutri_config = {
+#     "query": query,
+# }
+# request = requests.post(
+#     url=nutri_endpoint,
+#     json=nutri_config,
+#     headers=headers,
+# )
+# print(request.text)
