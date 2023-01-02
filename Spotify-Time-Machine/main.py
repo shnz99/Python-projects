80a812bbd73f74b40521908c5826ec783b0e1a73
damiangaw <d.gawin@pm.me>
2023-01-02 22:03:37 +0100

    list of spotify urls fromm billboard 100

--- Spotify-Time-Machine/main.py
+++ Spotify-Time-Machine/main.py
@@ -3,30 +3,42 @@ import requests
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
-# Which year do you want to travel to? Type the date in this format YYYY-MM-DD:
 YEAR = input(
     "Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n"
 )
+
 spotify_id = os.environ.get("SPOTIFY_ID")
 spotify_secret = os.environ.get("SPOTIFY_SECRET")
 spotify_user = os.environ.get("SPOTIFY_USER")
 
+pp = pprint.PrettyPrinter()
 auth_manager = SpotifyOAuth(
     client_id=spotify_id,
     client_secret=spotify_secret,
     redirect_uri="http://example.com",
     scope="playlist-modify-private",
 )
-
 sp = spotipy.Spotify(auth_manager=auth_manager)
+list_of_urls = []
 
+###################################### download 100 songs from desired time #########################################################
 response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{YEAR}/")
-contents = response.text
-soup = BeautifulSoup(contents, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
 titles = soup.select(".chart-results-list .o-chart-results-list__item h3")
+###################################### #################################### #########################################################
 
-with open("100songs.txt", mode="+w") as file:
-    for title in titles:
-        file.write(f"{title.getText().strip()}\n")
+for title in titles:
+    result = sp.search(
+        q=f"track: {title.getText().strip()} year: {YEAR[:4]}",
+        limit=1,
+        offset=0,
+        type="track",
+        market=None,
+    )
+    # pp.pprint(result)
+    list_of_urls.append(result["tracks"]["items"][0]["uri"])
+
+print(list_of_urls)
