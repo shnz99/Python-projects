6c59810e56398e6b55efb257012242e38ec87009
damiangaw <d.gawin@pm.me>
2023-01-19 12:11:57 +0100

    simple blog with flask

--- blog-flask-app/main.py
+++ blog-flask-app/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(blog_url)
+all_posts = response.json()
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def home():    
+    return render_template("index.html", posts=all_posts)
+
+@app.route("/blog/<int:id>")
+def blog(id):
+    for blog_post in all_posts:
+        if id == blog_post["id"]:
+            post = blog_post
+            return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
